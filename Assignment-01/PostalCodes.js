a43cbd310095e471409d975273895a17fb048cc8
Nahid191 <nahiduli785@gmail.com>
2022-11-05 17:40:40 +0600

    first commit

---
+++ Assignment-01/PostalCodes.js
@@ -0,0 +1,22 @@
+let PostalCodeslist=[
+    {District:'Dhaka', Area:'Motijheel', Postal_codes:1222/23 },
+    {District:'Dhaka', Area:'Mohammadpur', Postal_codes:1207/1225 },
+    {District:'Dhaka', Area:'Mirpur', Postal_codes: 1216},
+    {District:'Dhaka', Area:'Gulshan', Postal_codes:1212/13 },
+    {District:'Dhaka', Area:'Dhanmondi', Postal_codes:1209 },
+    {District:'Dhaka', Area:'Paltan', Postal_codes: 1000 },
+    {District:'Savar', Area:'All', Postal_codes:1340-45},
+    {District:'Faridpur', Area:'Faridpur City', Postal_codes:7800-3 },
+    {District:'Gazipur', Area:'Gazipur City', Postal_codes:1700-4 },
+    {District:'Gopalganj', Area:'Gopalganj City', Postal_codes:8100-8102/8013 },
+    {District:'Kishoreganj', Area:'Kishoreganj City', Postal_codes:2300-3 },
+    {District:'Madaripur', Area:'Madaripur City', Postal_codes:7900-7904 },
+    {District:'Manikganj', Area:'Manikganj City', Postal_codes: 1800-1804},
+    {District:'Munshiganj', Area:'Munshiganj City', Postal_codes:1500-1503 },
+    {District:'Narayanganj', Area:'Narayanganj City', Postal_codes:1400 },
+    {District:'Narsingdi', Area:'Narsingdi City', Postal_codes:1600-5 },
+    {District:'Rajbari', Area:'Rajbari City', Postal_codes:	7700/7710-11 },
+    {District:'Shariatpur', Area:'Shariatpur City', Postal_codes:8000-2},
+
+]
+export {PostalCodeslist};
---
+++ Assignment-01/main.js
@@ -0,0 +1,6 @@
+import {PostalCodeslist} from "./PostalCodes.js";
+
+PostalCodeslist.map((item)=>{
+    let Details=`PostalCodes District is ${item.District}`
+    console.log(Details)
+})
